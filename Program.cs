7b2dc28205ea58af4afb8bce4b68e6a0ac6deae6
NinaAlejandra <niinaa___@hotmail.com>
2023-09-12 16:42:26 +0200

    added in comment

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// uppgift 1: klasserna employee och program bör finnas med när man lagrar flera anställda. Just nu är enbart program med. 
-//uppgift 2: name, salary och wage är attributerna. Eventuella metoder Parse, WriteLine och ReadLine, men inga egna, som eventuellt AddEmployee som skulle vara bra att skapa.   
+//uppgift 2: name, salary och wage är attributerna. Eventuella metoder Parse, ToUpper, Substring, WriteLine och ReadLine, men inga egna, som eventuellt AddEmployee som skulle vara bra att skapa.   
 
 Console.WriteLine("Ange förnamn på anställd:");
 string fName = Console.ReadLine();
