72b058f19dd24957db7aec99115a5d50e32e31d9
NinaAlejandra <niinaa___@hotmail.com>
2023-09-12 16:39:47 +0200

    adding capitalization for firstname and lastname

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿// uppgift 1: klasserna employee och program bör finnas med när man lagrar flera anställda. Just nu är enbart program med. 
 //uppgift 2: name, salary och wage är attributerna. Eventuella metoder Parse, WriteLine och ReadLine, men inga egna, som eventuellt AddEmployee som skulle vara bra att skapa.   
 
-Console.WriteLine("Ange för- och efternamn på anställd:");
-string name = Console.ReadLine();
+Console.WriteLine("Ange förnamn på anställd:");
+string fName = Console.ReadLine();
+fName = char.ToUpper(fName[0]) + fName.Substring(1);
+
+Console.WriteLine("Ange efternamn på anställd:");
+string lName = Console.ReadLine();
+lName = char.ToUpper(lName[0]) + lName.Substring(1);
 
 Console.WriteLine("Ange lön för anställd:");
 string salary = Console.ReadLine();
 double wage = double.Parse(salary);
 
-Console.WriteLine("Anställd heter " + name + " och tjänar " + wage + " i månaden");
+Console.WriteLine("Anställd heter " + fName + (" ") + lName + " och tjänar " + wage + " i månaden");
